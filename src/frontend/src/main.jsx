fb93a8cd84072d02d551baa8ac0401a0b0ea1693
venkatsaireddy19191 <patlollavenkatsai@gmail.com>
2026-02-22 16:04:05 +0530

    changed the main.jsx and added the newFile

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useEffect } from "react";
+import { useContext } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
